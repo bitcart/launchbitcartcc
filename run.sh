74e8a2d00f016bd09bcdf8b327a3f6ebc4a08a40
MrNaif2018 <chuff184@gmail.com>
2020-01-17 20:21:42 +0300

    Initial commit

---
+++ run.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+cat <<EOM >/root/run.py
+import os
+import subprocess
+import sys
+import time
+
+hostname = "[HOSTNAME]"
+network = "[NETWORK]"
+email = "[EMAIL]"
+store_email = "[STOREEMAIL]"
+store_pass = "[STOREPASS]"
+store_id = "[STOREID]"
+repository = "[REPOSITORY]"
+branch = "[BRANCH]"
+coinstr = "[COINS]"
+lightning = "[LIGHTNING]"
+
+reverseproxy = "nginx-https"
+coinmap = ["btc", "bch", "ltc", "gzro", "bsty"]
+
+# clone bitcart-docker
+if not os.path.exists("/root/bitcart-docker"):
+    subprocess.call(["git", "clone", repository, "bitcart-docker"], cwd="/root/")
+    subprocess.call(["git", "checkout", branch], cwd="/root/bitcart-docker")
+
+env = os.environ.copy()
+coins = coinstr.split(",")
+
+for coin in coins:
+    if coin in coinmap:
+        env["{}_NETWORK".format(coin)] = network
+        env["{}_LIGHTNING".format(coin)] = lightning
+
+env["BITCART_CRYPTOS"] = coinstr
+env["BITCART_REVERSEPROXY"] = reverseproxy
+env["BITCART_HOST"] = "api{}".format(hostname)
+env["BITCART_ADMIN_HOST"] = "admin{}".format(hostname)
+env["BITCART_FRONTEND_HOST"] = hostname
+env["BITCART_ADMIN_URL"] = "https://api{}".format(hostname)
+env["BITCART_FRONTEND_URL"] = "https://api{}".format(hostname)
+env["BITCART_FRONTEND_EMAIL"] = store_email
+env["BITCART_FRONTEND_PASSWORD"] = store_pass
+env["BITCART_FRONTEND_STORE"] = store_id
+env["BITCART_LETSENCRYPT_EMAIL"] = email
+
+for i in range(5):
+    popen = subprocess.Popen(
+        ["bash", "-c", ". ./setup.sh"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        env=env,
+        cwd="/root/bitcart-docker",
+    )
+    had_error = False
+    for line in popen.stdout:
+        sys.stdout.buffer.write(b"[bitcart-setup] ")
+        sys.stdout.buffer.write(line)
+        if (
+            b"Could not resolve host:" in line
+            or b"docker-compose: command not found" in line
+        ):
+            had_error = True
+    popen.stdout.close()
+    return_code = popen.wait()
+    if return_code == 0 and not had_error:
+        subprocess.Popen(
+            ["bash", "-c", ". ./start.sh"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            env=env,
+            cwd="/root/bitcart-docker",
+        ).wait()
+        break
+    else:
+        print("launcher: bitcart-setup script had error, retrying in 10 seconds")
+        time.sleep(10)
+        continue
+EOM
+
+# for now this should be enough time to attach volumes
+# later on we may need to do something more robust
+sleep 20
+/usr/bin/python3 /root/run.py
+
+[ -x "$(command -v /etc/init.d/sshd)" ] && nohup /etc/init.d/sshd restart &
+[ -x "$(command -v /etc/init.d/ssh)" ] && nohup /etc/init.d/ssh restart &
