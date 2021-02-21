6d18aa19d9f9630b68b2ca7f78bcfb44ae595f84
MrNaif2018 <chuff184@gmail.com>
2021-02-21 22:32:07 +0300

    Rename API env variables

--- run.sh
+++ run.sh
@@ -39,8 +39,8 @@ if custom_domain:
 env["BITCART_HOST"] = "api{delim}{}".format(hostname, delim=delim)
 env["BITCART_ADMIN_HOST"] = "admin{delim}{}".format(hostname, delim=delim)
 env["BITCART_STORE_HOST"] = hostname
-env["BITCART_ADMIN_URL"] = "https://api{delim}{}".format(hostname, delim=delim)
-env["BITCART_STORE_URL"] = "https://api{delim}{}".format(hostname, delim=delim)
+env["BITCART_ADMIN_API_URL"] = "https://api{delim}{}".format(hostname, delim=delim)
+env["BITCART_STORE_API_URL"] = "https://api{delim}{}".format(hostname, delim=delim)
 env["BITCART_LETSENCRYPT_EMAIL"] = email
 
 for i in range(5):
